ea08899fff81bd76b5964bad46e2a243f0d14025
Muhammad Ibrahim <116445625+mibrahim76112@users.noreply.github.com>
2025-02-13 14:45:16 -0600

    Update task2.cpp

--- task1/task2.cpp
+++ task1/task2.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    int firstUniqChar(string s) {
+    map<char, int> kv;  
+    for (auto i : s) {
+        kv[i] = kv[i] + 1;  
+        cout << i;
+    }
+     for (char i : s) {
+        if (kv[i] == 1)
+        {
+            return s.find(i);
+        }
+    }
+    return -1;
+    }
+};
